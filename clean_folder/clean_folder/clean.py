67ff8c13f5249737657a63c9d4b93a8c5420f633
YegorDav <yegor13091991@gmail.com>
2023-04-20 13:29:33 +0300

    finish

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -1,60 +1,139 @@
 import os
+import sys
+import shutil
 
-# список розширень для кожної категорії
-IMAGE_EXT = ('JPEG', 'JPG', 'PNG', 'SVG')
-VIDEO_EXT = ('AVI', 'MP4', 'MOV', 'MKV')
-DOCUMENT_EXT = ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX')
-MUSIC_EXT = ('MP3', 'OGG', 'WAV', 'AMR')
-ARCHIVE_EXT = ('ZIP', 'GZ', 'TAR')
+# CONSTANT
+CATEGORIES = dict(IMAGE = ['jpeg', 'png', 'jpg', 'svg'], 
+                  VIDEO = ['avt', 'mp4', 'mov', 'mkv'],
+                  DOCS = ['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx','rtf'],
+                  MUSIC = ['mp3', 'ogg', 'wav', 'amr'],
+                  ARCHIVE = ['zip', 'gz', 'tar','7z'],
+                  OTHER = [])
+IGNORE = ['.DS_Store']
 
-# функція для перевірки розширення файлу та повернення категорії
-def get_category(filename):
-    ext = os.path.splitext(filename)[1][1:].upper()
-    if ext in IMAGE_EXT:
-        return 'images'
-    elif ext in VIDEO_EXT:
-        return 'videos'
-    elif ext in DOCUMENT_EXT:
-        return 'documents'
-    elif ext in MUSIC_EXT:
-        return 'music'
-    elif ext in ARCHIVE_EXT:
-        return 'archives'
-    else:
-        return 'unknown'
+# Parent Directory path
+sorted_path = sys.argv[1]
 
-# функція для нормалізації імен файлів
-def normalize(filename):
-    # Створюємо словник для транслітерації
-    translit_dict = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ie',
-                     'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l',
-                     'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
-                     'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'iu', 'я': 'ia'}
 
-    # Перетворюємо всі літери рядка в нижній регістр
-    filename = filename.lower()
+translate_dict = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'yo',
+      'ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n',
+      'о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'h',
+      'ц':'c','ч':'ch','ш':'sh','щ':'sch','ъ':'','ы':'y','ь':'','э':'e',
+      'ю':'u','я':'ya', 'А':'A','Б':'B','В':'V','Г':'G','Д':'D','Е':'E','Ё':'YO',
+      'Ж':'ZH','З':'Z','И':'I','Й':'I','К':'K','Л':'L','М':'M','Н':'N',
+      'О':'O','П':'P','Р':'R','С':'S','Т':'T','У':'U','Ф':'F','Х':'H',
+      'Ц':'C','Ч':'CH','Ш':'SH','Щ':'SCH','Ъ':'','Ы':'y','Ь':'','Э':'E',
+      'Ю':'U','Я':'YA','ґ':'','ї':'', 'є':'','Ґ':'g','Ї':'i',
+      'Є':'e','1':'1','2':'2','3':'3'}
 
-    # Проходимося по кожному символу у рядку та перевіряємо, чи є він латинською літерою або цифрою
-    # Якщо символ не є латинською літерою або цифрою, замінюємо його на "_"
-    normalized_filename = ''
-    for char in filename:
-        if char.isalpha() and char.isascii():
-            normalized_filename += char
-        elif char.isdigit():
-            normalized_filename += char
-        else:
-            normalized_filename += '_'
 
-    # Проходимося по кожній букві у рядку та замінюємо кириличні символи на латинські за допомогою словника
-    for cyrillic_char, latin_char in translit_dict.items():
-        normalized_filename = normalized_filename.replace(
-            cyrillic_char, latin_char)
+# Print list of file by category
+def file_list_by_folder():
+    for folder in os.listdir(sorted_path):
+        way = os.listdir(os.path.join(sorted_path, folder))
+        print('{:>50} - {:<50} '.format('Folder', folder))
+        print('{:^100}'.format('-'*99))
+        for file in way:
+            print('{:^5} {:<95} '.format('',file))
+        print('\n')    
+        print('{:^100}'.format('-'*99))
 
-    return
 
-    # функція для узагальненя визову
+# transliterate word function
+def translite(word):
+    for key in translate_dict:
+        word = word.replace(key, translate_dict[key])
+    return word
+
+
+# Rename file to transliterated name
+def normalize():
+    for el in os.listdir(sorted_path):
+        tel =  translite(el)
+        os.rename(os.path.join(sorted_path,el), os.path.join(sorted_path,tel))
+
+
+# Create set of founded extension
+def create_ext_set():
+    ext_set = set()
+    for el in os.listdir(sorted_path):
+        ext = el.split('.')[-1]
+        if os.path.isfile(os.path.join(sorted_path,el)) and ext in str(CATEGORIES.values()):
+            ext_set.add(ext)
+    return ext_set
+
+
+# Create set of founded unknown extension
+def create_un_ext_set():
+    ext_set = set()
+    for el in os.listdir(sorted_path):
+        ext = el.split('.')[-1]
+        if os.path.isfile(os.path.join(sorted_path,el)) and ext not in str(CATEGORIES.values()):
+            ext_set.add(ext)
+    return ext_set
+
+
+# Delete empty folders
+def del_empty_dirs(adress):
+    for dirs in os.listdir(adress):
+        dir = os.path.join(adress, dirs)
+        if os.path.isdir(dir):
+            del_empty_dirs(dir)
+            if not os.listdir(dir):
+                shutil.rmtree(dir)
+
+
+# Recursed folders extract
+def deep_folders(root_folder):
+    for subdir, dirs, files in os.walk(root_folder):
+        for file in files:
+            src = os.path.join(subdir, file)
+            dst = os.path.join(root_folder, file)
+            shutil.move(src, dst)
+        del_empty_dirs(root_folder)
+
+
+# Create category folders 
+def create_sort_folders(sorted_path):
+    for category in CATEGORIES:
+        if category not in os.listdir():
+            os.mkdir(os.path.join(sorted_path, category))
+
+
+def get_category(ext):
+    for category, extinsions in CATEGORIES.items():
+        if ext in extinsions and ext not in IGNORE:
+            return category
+    return "OTHER"
+
+# Sorting
+def sort(sorted_path):
+    for el in os.listdir(sorted_path):
+        if os.path.isfile(os.path.join(sorted_path, el)):
+            ext = el.split('.')[-1]
+            category = get_category(ext)
+            target_folder = os.path.join(sorted_path, category)
+            if category == "ARCHIVE":
+                shutil.unpack_archive( os.path.join(sorted_path, el), target_folder)
+                os.remove(os.path.join(sorted_path,el))
+                continue
+            shutil.move(os.path.join(sorted_path,el), target_folder)
+
+
+#Main function
 def main():
-    get_category()
+    deep_folders(sorted_path)
+    with_ext = create_ext_set()
+    without_ext = create_un_ext_set()
     normalize()
-if __name__ == '__main__':
+    create_sort_folders(sorted_path)
+    sort(sorted_path)
+    file_list_by_folder()
+    print('{:^100}'.format('*'*100))
+    print(f"Founded file's with know extension: {with_ext}")
+    print(f"Founded file's with unknown extension: {without_ext}")
+    print('{:^100}'.format('*'*100))
+
+
+if __name__ == "__main__" :
     main()
